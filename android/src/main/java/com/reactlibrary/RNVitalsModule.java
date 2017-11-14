5495b483e6cb3739d34dfbe907967ad4cd084c91
Ying Hang Eng <eyinghang@gmail.com>
2017-11-14 13:44:07 -0500

    remove random line

--- android/src/main/java/com/reactlibrary/RNVitalsModule.java
+++ android/src/main/java/com/reactlibrary/RNVitalsModule.java
@@ -10,7 +10,6 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.WritableMap;
-import com.sun.xml.internal.ws.api.Component;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
