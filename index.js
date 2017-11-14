c5aa2c97addd4ed34f0372861d4637c9690d83ce
Ying Hang Eng <eyinghang@gmail.com>
2017-11-14 13:46:53 -0500

    listener for low memory on android

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-import {NativeModules} from 'react-native';
+'use strict';
+
+import {
+  DeviceEventEmitter,
+  NativeModules
+} from 'react-native';
 
 const {RNVitals} = NativeModules;
 
-export default RNVitals;
+export default {
+  ...RNVitals,
+  addListener (callback) {
+    return DeviceEventEmitter.addListener(
+      MobileDeviceManager.LOW_MEMORY,
+      callback
+    );
+  }
+};
